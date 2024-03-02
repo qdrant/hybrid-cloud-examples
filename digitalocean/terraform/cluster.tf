5870b8658d4a4b6e23825d652c7dc82732202101
Volodymyr Zholudiev <volodymyr.zholudiev@deliveryhero.com>
2024-03-02 13:37:47 +0100

    add kubeconfig

--- digitalocean/terraform/cluster.tf
+++ digitalocean/terraform/cluster.tf
@@ -36,3 +36,9 @@ data "digitalocean_sizes" "k8s" {
   }
 
 }
+
+
+resource "local_file" "kubeconfig" {
+  content  = digitalocean_kubernetes_cluster.qdrant_example.kube_config.0.raw_config
+  filename = "kubeconfig"
+}
