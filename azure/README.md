183e3a18afc381d7c6ab216100e0372104132d1d
Volodymyr Zholudiev <volodymyr.zholudiev@deliveryhero.com>
2024-04-18 09:03:29 +0200

    feat: Add Azure support

---
+++ azure/README.md
@@ -0,0 +1,11 @@
+# Qdrant Hybrid Cloud Example for Google Cloud
+
+Example how to use Azure k8s cluster (AKS) as private region for hybrid qdrant cloud.
+
+### Prerequisites
+
+- You have [installed](https://learn.microsoft.com/en-us/cli/azure/install-azure-cli-macos#install-with-homebrew) `Azure` cli tool
+- You configure `az`
+    - `az login`
+    - ` gcloud auth application-default login --project <your-project-name>`
+- 
---
+++ azure/terraform/vnet.tf
@@ -0,0 +1,21 @@
+resource "azurerm_resource_group" "terraform_infra" {
+  name     = format("%s-rg", var.resource_name)
+  location = var.region
+  tags     = var.tags
+}
+
+module "vnet" {
+  source     = "squareops/vnet/azurerm"
+
+  name                         = var.vpc_name
+  address_space                = var.vpc_cidr
+  environment                  = var.environment
+
+  create_resource_group        = false                                       # Enable if you want to a create resource group for AKS cluster.
+  existing_resource_group_name = azurerm_resource_group.terraform_infra.name # We will be using resource group create earlier.
+  resource_group_location      = var.region
+  create_vpn                   = false
+  create_nat_gateway           = true
+  logging_enabled              = false
+  additional_tags              = var.tags
+}
